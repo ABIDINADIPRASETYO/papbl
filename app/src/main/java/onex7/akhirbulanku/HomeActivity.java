36276d8a0dc334d98d6421e62d069630c68ea968
Wiandono Saputro <wiandonosaputro@gmail.com>
2018-05-09 16:16:13 +0700

    Menu navbar fix

--- app/src/main/java/onex7/akhirbulanku/HomeActivity.java
+++ app/src/main/java/onex7/akhirbulanku/HomeActivity.java
@@ -102,18 +102,17 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            finish();
+            startActivity(new Intent(HomeActivity.this, HomeActivity.class));
+        } else if (id == R.id.nav_dashboard) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
